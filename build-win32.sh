d6096affe43a913a038a04b40023df09e806a28f
Adam <Adam@sigterm.info>
2019-08-02 10:21:50 -0400

    win32 script - need to figure out what dlls are required

--- build-win32.sh
+++ build-win32.sh
@@ -5,19 +5,19 @@ set -e
 JDK_VER="11.0.4"
 JDK_BUILD="11"
 
-if ! [ -f OpenJDK11U-jre_x64_windows_hotspot_${JDK_VER}_${JDK_BUILD}.zip ] ; then
-    curl -Lo OpenJDK11U-jre_x64_windows_hotspot_${JDK_VER}_${JDK_BUILD}.zip \
-        https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-${JDK_VER}%2B${JDK_BUILD}/OpenJDK11U-jre_x64_windows_hotspot_${JDK_VER}_${JDK_VER}.zip
+if ! [ -f OpenJDK11U-jre_x86-32_windows_hotspot_${JDK_VER}_${JDK_BUILD}.zip ] ; then
+    curl -Lo OpenJDK11U-jre_x86-32_windows_hotspot_${JDK_VER}_${JDK_BUILD}.zip \
+        https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-${JDK_VER}%2B${JDK_BUILD}/OpenJDK11U-jre_x86-32_windows_hotspot_${JDK_VER}_${JDK_BUILD}.zip
 fi
 
-echo "be88c679fd24194bee71237f92f7a2a71c88f71a853a56a7c05742b0e158c1be OpenJDK11U-jre_x64_windows_hotspot_${JDK_VER}_${JDK_BUILD}.zip" | sha256sum -c
+echo "24eb66c5858c09c58a50b51df98b8dd6f75151a2b9f8e2e822441fa9f29009b6 OpenJDK11U-jre_x86-32_windows_hotspot_${JDK_VER}_${JDK_BUILD}.zip" | sha256sum -c
 
 # packr requires a "jdk" and pulls the jre from it - so we have to place it inside
 # the jdk folder at jre/
-if ! [ -d win64-jdk ] ; then
-    unzip OpenJDK11U-jre_x64_windows_hotspot_${JDK_VER}_${JDK_BUILD}.zip
-    mkdir win64-jdk
-    mv jdk-11.0.4+11-jre win64-jdk/jre
+if ! [ -d win32-jdk ] ; then
+    unzip OpenJDK11U-jre_x86-32_windows_hotspot_${JDK_VER}_${JDK_BUILD}.zip
+    mkdir win32-jdk
+    mv jdk-11.0.4+11-jre win32-jdk/jre
 fi
 
 if ! [ -f packr.jar ] ; then
@@ -28,9 +28,9 @@ echo "5825a18196a813158e247351e0b43a10c521bf227c931f41e536e61b085f2b80 packr.jar
 
 java -jar packr.jar \
     --platform \
-    windows64 \
+    windows32 \
     --jdk \
-    win64-jdk \
+    win32-jdk \
     --executable \
     RuneLite \
     --classpath \
@@ -44,13 +44,13 @@ java -jar packr.jar \
     XX:CompileThreshold=1500 \
     Djna.nosys=true \
     --output \
-    native-win64
+    native-win32
 
 # packr on Windows doesn't support icons, so we use resourcehacker to include it
 
 resourcehacker \
-    -open native-win64/RuneLite.exe \
-    -save native-win64/RuneLite.exe \
+    -open native-win32/RuneLite.exe \
+    -save native-win32/RuneLite.exe \
     -action add \
     -res runelite.ico \
     -mask ICONGROUP,MAINICON,
@@ -58,4 +58,4 @@ resourcehacker \
 # XXX here we update the manifest for DPI scaling - is that required still?
 
 # We use the filtered iss file
-iscc target/filtered-resources/runelite.iss
+iscc target/filtered-resources/runelite32.iss
