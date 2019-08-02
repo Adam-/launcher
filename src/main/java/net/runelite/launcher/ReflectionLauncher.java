8768509608ebcaa7d1babd153cea972194001169
Adam <Adam@sigterm.info>
2019-08-02 09:57:17 -0400

    fixup checkstyle

--- src/main/java/net/runelite/launcher/ReflectionLauncher.java
+++ src/main/java/net/runelite/launcher/ReflectionLauncher.java
@@ -25,7 +25,6 @@
 package net.runelite.launcher;
 
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
