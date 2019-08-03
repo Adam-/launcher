aa04de7c090a667534bfffd87af44ac64441a6e5
Adam <Adam@sigterm.info>
2019-08-02 22:26:07 -0400

    Fix up osx

--- build-osx.sh
+++ build-osx.sh
@@ -10,7 +10,7 @@ if ! [ -f OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz ] ; then
         https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-${JDK_VER}%2B${JDK_BUILD}/OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz
 fi
 
-echo "1647fded28d25e562811f7bce2092eb9c21d30608843b04250c023b40604ff26 OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz" | sha256sum -c
+echo "1647fded28d25e562811f7bce2092eb9c21d30608843b04250c023b40604ff26  OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz" | shasum -c
 
 # packr requires a "jdk" and pulls the jre from it - so we have to place it inside
 # the jdk folder at jre/
@@ -18,12 +18,12 @@ if ! [ -d osx-jdk ] ; then
     tar zxf OpenJDK11U-jre_x64_mac_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz
     mkdir osx-jdk
     mv jdk-11.0.4+11-jre osx-jdk/jre
-fi
 
-# Move JRE out of Contents/Home/
-pushd osx-jdk/jre
-cp -r Contents/Home/* .
-popd
+    # Move JRE out of Contents/Home/
+    pushd osx-jdk/jre
+    cp -r Contents/Home/* .
+    popd
+fi
 
 java -jar packr.jar \
     --platform \
@@ -54,6 +54,6 @@ pushd native-osx/RuneLite.app
 chmod g+x,o+x Contents/MacOS/RuneLite
 popd
 
-# createdmg exits with an error code on success
+# create-dmg exits with an error code due to no code signing, but is still okay
 # note we use Adam-/create-dmg as upstream does not support UDBZ
-createdmg --format UDBZ native-osx/RuneLite.app || true
+create-dmg --format UDBZ native-osx/RuneLite.app || true
