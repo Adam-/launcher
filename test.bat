68d3e05c3c51f5b202c9aa5507f6bf5a34c0cbe2
Adam <Adam@sigterm.info>
2022-08-10 10:48:18 -0400

    .

--- test.bat
+++ test.bat
@@ -1,26 +1,16 @@
-# modify packr exe manifest to enable Windows dpi scaling
-resourcehacker \
-    -open native-win64/RuneLite.exe \
-    -save native-win64/RuneLite.exe \
-    -action addoverwrite \
-    -res packr/runelite.manifest \
-    -log rh.log \
+rem modify packr exe manifest to enable Windows dpi scaling
+resourcehacker ^
+    -open native-win64/RuneLite.exe ^
+    -save native-win64/RuneLite.exe ^
+    -action addoverwrite ^
+    -res packr/runelite.manifest ^
     -mask MANIFEST,1,
 
-echo rh log manifest start
-cat rh.log
-rm -f rh.log
-
-# packr on Windows doesn't support icons, so we use resourcehacker to include it
-
-mv native-win64/RuneLite.exe native-win64/RuneLite-orig.exe
-resourcehacker \
-    -open native-win64/RuneLite-orig.exe \
-    -save native-win64/RuneLite.exe \
-    -action add \
-    -res runelite.ico \
-    -log rh.log \
+rem packr on Windows doesn't support icons, so we use resourcehacker to include it
+move native-win64/RuneLite.exe native-win64/RuneLite-orig.exe
+resourcehacker ^
+    -open native-win64/RuneLite-orig.exe ^
+    -save native-win64/RuneLite.exe ^
+    -action add ^
+    -res runelite.ico ^
     -mask ICONGROUP,MAINICON,
-
-echo RH LOG START
-cat rh.log
