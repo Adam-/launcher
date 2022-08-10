ac088e2d3777c201975feeec8fbc1c15afcbd023
Adam <Adam@sigterm.info>
2022-08-10 10:51:53 -0400

    dir

--- test.bat
+++ test.bat
@@ -1,9 +1,14 @@
+dir
+
+dir native-win64
+
 rem modify packr exe manifest to enable Windows dpi scaling
 resourcehacker ^
     -open native-win64/RuneLite.exe ^
     -save native-win64/RuneLite.exe ^
     -action addoverwrite ^
     -res packr/runelite.manifest ^
+    -log CONSOLE ^
     -mask MANIFEST,1,
 
 rem packr on Windows doesn't support icons, so we use resourcehacker to include it
@@ -13,4 +18,5 @@ resourcehacker ^
     -save native-win64/RuneLite.exe ^
     -action add ^
     -res runelite.ico ^
+    -log CONSOLE ^
     -mask ICONGROUP,MAINICON,
